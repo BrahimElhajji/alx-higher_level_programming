6b841c0c501c2721bba95514ac09ac8081694b2c
BrahimElhajji <elhajji080898@gmail.com>
2024-01-02 16:47:51 -0800

    all mendatory tasks

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * check_cycle - Checks if the list contains a cycle.
+ * @list: A singly-linked list.
+ *
+ * Return: 1 If there is no cycle and 0 If there is a cycle.
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t quick, *lazy;
+
+	if (list == NULL || list->next == NULL)
+		return (0);
+
+	while (quick && quick->next)
+	{
+		lazy = lazy->next;
+		quick = quick->next->next;
+
+		if (lazy == quick)
+			return (1);
+
+	}
+
+	return (0);
+}
