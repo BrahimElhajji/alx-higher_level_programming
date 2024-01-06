550b25b69567681f09e3fef030e7417047a72e09
BrahimElhajji <elhajji080898@gmail.com>
2024-01-05 20:36:52 -0800

    tasks from 1 to 6

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+def no_c(my_string):
+
+    without_c = ""
+    c_string = my_string
+    for c in c_string:
+        if c.lower() != 'c':
+            without_c += c    
+    return without_c
