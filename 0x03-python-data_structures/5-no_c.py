d0d57f0b77269bb0aabb245c59615cbcb8d6ed13
BrahimElhajji <elhajji080898@gmail.com>
2024-01-06 13:29:54 -0800

    fixed task 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -5,5 +5,5 @@ def no_c(my_string):
     c_string = my_string
     for c in c_string:
         if c.lower() != 'c':
-            without_c += c    
+            without_c += c 
     return without_c
