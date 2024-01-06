8f9671b2ee2f9b605fbece9145978a51aee14f65
BrahimElhajji <elhajji080898@gmail.com>
2024-01-06 15:07:20 -0800

    task 10

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -3,8 +3,8 @@
 def divisible_by_2(my_list=[]):
     o_list = []
     for i in my_list:
-        if my_list[i] % 2 == 0:
+        if i % 2 == 0:
             o_list.append(True)
-        elif my_list[i] % 2 == 1:
+        elif i % 2 == 1:
             o_list.append(False)
     return o_list
