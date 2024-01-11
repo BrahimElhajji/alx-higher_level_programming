b10e2e5980967e017fa69ed0be6c9e835c05ee53
BrahimElhajji <elhajji080898@gmail.com>
2024-01-10 21:05:31 -0800

    fixed task 15

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
 
-    for k in a_dictionary.keys():
+    for k in a_dictionary.items():
         if k == value:
-            del a_dictionary
+            del a_dictionary[k]
     return a_dictionary
