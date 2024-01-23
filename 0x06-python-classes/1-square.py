5d3a6645f645a6fff11bc37c51377493ce801c6d
BrahimElhajji <elhajji080898@gmail.com>
2024-01-23 10:04:41 -0800

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""class for square"""
+
+
+class Square(object):
+    """class that represent a square
+
+    Initialing a instance of the Square clas"""
+    def __init__(self, size):
+        self.__size = size
