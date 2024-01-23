fde29fc1e7db88526f5864f8396ac7fe417c4e62
BrahimElhajji <elhajji080898@gmail.com>
2024-01-23 15:06:15 -0800

    task 4

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -9,23 +9,23 @@ class Square(object):
     def __init__(self, size=0):
         self.__size = size
 
-        @property
-        def size(self):
-            """Getter to retrieve the size attribute"""
-        return self.__size
+    @property
+    def size(self):
+        """Getter to retrieve the size attribute"""
+    return self.__size
 
-        @size.setter
-        def size(self, value):
-            """Setter to set the size attribute with type
-                and value validation"""
-            """Check if value is an integer"""
-            if type(value) != int:
-                raise TypeError("size must be an integer")
-                """Check if value is less than 0"""
-                if value < 0:
-                    raise ValueError("size must be >= 0")
-                self.__size = value
+    @size.setter
+    def size(self, value):
+        """Setter to set the size attribute with type
+            and value validation"""
+        """Check if value is an integer"""
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+            """Check if value is less than 0"""
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
 
-        def area(self):
-            """Calculate and return the area of the square"""
-        return self.__size * self.__size
+    def area(self):
+        """Calculate and return the area of the square"""
+    return self.__size * self.__size
