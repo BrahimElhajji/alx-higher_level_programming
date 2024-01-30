8dea84a44ef98a2520e02914caab8251dc57eb57
BrahimElhajji <elhajji080898@gmail.com>
2024-01-29 20:55:29 -0800

    damn

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -31,14 +31,14 @@ class Rectangle():
         return self.__height
 
     @height.setter
-    def height(self, height):
+    def height(self, value):
         """Setter method for the height attribute."""
-        if not isinstance(height, int):
-            raise TypeError("width must be an integer")
-        if height < 0:
-            raise ValueError("idth must be >= 0")
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
         else:
-            self.__height = height
+            self.__height = value
 
     @property
     def width(self):
@@ -46,14 +46,14 @@ class Rectangle():
         return self.__width
 
     @width.setter
-    def width(self, width):
+    def width(self, value):
         """Setter method for the height attribute."""
-        if not isinstance(width, int):
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        if width < 0:
-            raise ValueError("idth must be >= 0")
+        if value < 0:
+            raise ValueError("width must be >= 0")
         else:
-            self.__width = width
+            self.__width = value
 
     def __str__(self):
         """create a string represent the rectangle 
