9e1129157411898cfbefd73ebce193231540281d
BrahimElhajji <elhajji080898@gmail.com>
2024-01-29 21:16:07 -0800

    ullah ma3reft

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -87,15 +87,12 @@ class Rectangle():
         """
         Static method that returns the rectangle with the larger area.
         """
-        area_1 = rect_1.area()
-        area_2 = rect_2.area()
-
         if not isinstance(rect_1, Rectangle):
             raise TypeError("rect_1 must be an instance of Rectangle")
         if not isinstance(rect_2, Rectangle):
             raise TypeError("rect_2 must be an instance of Rectangle")
 
-        if area_1 >= area_2:
+        if rect_1.area() >= rect_2.area():
             return rect_1
         else:
             return rect_2
