413fd9ebd49d8d80cb77dd6456557cae7513b5b6
BrahimElhajji <elhajji080898@gmail.com>
2024-01-29 20:39:39 -0800

    fuck

--- 0x08-python-more_classes/5-rectangle.py
+++ 0x08-python-more_classes/5-rectangle.py
@@ -5,14 +5,13 @@
 class Rectangle():
     """A class representing a rectangle."""
 
-    def __init__ (self, width=0, height=0):
+    def __init__(self, width=0, height=0):
         """Attributes:
-            width: is The width of the rectangle.
-            height: is The height of the rectangle."""
+        width: is The width of the rectangle.
+        height: is The height of the rectangle."""
 
-            self.height = height
-            self.width = width
-
+        self.height = height
+        self.width = width
 
     def area(self):
         """Calculates the area of the rectangle."""
@@ -25,7 +24,7 @@ class Rectangle():
         else:
             return (self.__width + self.__height) * 2
 
-    @property    
+    @property
     def height(self):
         """Getter method for the height attribute."""
         return self.__height
@@ -56,7 +55,7 @@ class Rectangle():
             self.__width = width
 
     def __str__(self):
-        """create a string represent the rectangle 
+        """create a string represent the rectangle
         using the character '#'."""
         if self.__width == 0 or self.__height == 0:
             return ""
@@ -67,7 +66,7 @@ class Rectangle():
             return rectangle_str.rstrip()
 
     def __repr__(self):
-        """create a string that represent the rectangle 
+        """create a string that represent the rectangle
         for recreating a new instance using eval()."""
         rec = f"Rectangle(width={self.__width}, height={self.__height})"
         return rec
