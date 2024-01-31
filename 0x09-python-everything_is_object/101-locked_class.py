90f78b5851446906fe4fe74de32842278a4ffa64
BrahimElhajji <elhajji080898@gmail.com>
2024-01-30 16:34:39 -0800

    second advanced task

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+"""a class with no class or object attribute"""
+
+
 class LockedClass():
+    """calss that prevents the user from dynamically
+    creating new instance attributes"""
     __slots__ = ['first_name']
 
     def __init__(self):
