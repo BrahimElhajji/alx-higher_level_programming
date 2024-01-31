8dddbce407609dca338402af04c55c264b3fa033
BrahimElhajji <elhajji080898@gmail.com>
2024-01-31 11:07:39 -0800

    test

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -6,39 +6,39 @@ max_integer = __import__('6-max_integer').max_integer
 
 
 class TestMaxInteger(unittest.TestCase):
-    def empty_list(self):
+    def test_empty_list(self):
         output = max_integer([])
         self.assertIsNone(output)
 
-    def positive_numbers(self):
+    def test_positive_numbers(self):
         numbers = [1, 2, 3, 4, 5]
         output = max_integer(numbers)
         self.assertEqual(output, 5)
 
-    def negative_numbers(self):
+    def test_negative_numbers(self):
         numbers = [-5, -3, -1, -7]
         output = max_integer(numbers)
         self.assertEqual(output, -1)
 
-    def different_numbers(self):
+    def test_different_numbers(self):
         numbers = [10, -2, 5, -8, 15]
         output = max_integer(numbers)
         self.assertEqual(output, 15)
 
-    def duplicate_max_numbers(self):
+    def test_duplicate_max_numbers(self):
         numbers = [3, 8, 5, 3, 8, 10]
         output = max_integer(numbers)
         self.assertEqual(output, 10)
 
-    def one_element(self):
+    def test_one_element(self):
         output = max_integer([42])
         self.assertEqual(output, 42)
 
-    def default_arg(self):
+    def test_default_arg(self):
         output = max_integer()
         self.assertIsNone(output)
 
-    def invalid_input(self):
+    def test_invalid_input(self):
         with self.assertRaises(TypeError):
             max_integer(1, "lovely")
 
