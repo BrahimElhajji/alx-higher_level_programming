c9ad25d2ae5c539ad31716a3c9e505b92779b0ae
BrahimElhajji <elhajji080898@gmail.com>
2024-02-05 14:27:08 -0800

    task 8

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
-"""class Rectangle that inherits from BaseGeometry (7-base_geometry.py)"""
+"""class Rectangle that inherits BaseGeometry (7-base_geometry.py)"""
 
 
 class Rectangle(BaseGeometry):
