db5b9fb237ca98f41b9108fd39f355c546588c3e
BrahimElhajji <elhajji080898@gmail.com>
2024-02-05 04:38:16 -0800

    task 0

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-def lookup(obj):
-    """function that returns the list of
+"""function that returns the list of
     available attributes and methods of an object"""
+def lookup(obj):
+    """Returns:
+    - List of strings: Names of attributes
+    and methods of the object"""
     return(dir(obj))
