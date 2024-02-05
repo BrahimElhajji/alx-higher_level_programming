a742084af3d4ec5d1380adadf9573a51d8dc5b59
BrahimElhajji <elhajji080898@gmail.com>
2024-02-05 14:39:47 -0800

    task 10

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""class Square that inherits from Rectangle (9-rectangle.py)"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Rectangle(BaseGeometry):
+    """Instantiation with size"""
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        return self.__width * self.__height
+
+    def __str__(self):
+        return f"[Rectangle] {self.__width}/{self.__height}"
