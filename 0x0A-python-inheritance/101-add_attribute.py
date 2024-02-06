7ba282e738ee00afffddf1047b3f583fe64b10d4
BrahimElhajji <elhajji080898@gmail.com>
2024-02-05 16:07:34 -0800

    second advanced task

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -5,8 +5,8 @@
 def add_attribute(obj, name, value):
     """checks if it's possible to add a new attribute.
     """
-    if hasattr(obj, '__dict__') or (hasattr(type(obj), '__slots__') and
-        name in type(obj).__slots__):
+    if hasattr(obj, '__dict__') or (hasattr(type(obj), '__slots__')
+       and name in type(obj).__slots__):
         setattr(obj, name, value)
     else:
         raise TypeError("can't add new attribute")
