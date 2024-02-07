64bf10f4a83cc33461a135b896a787de897bf997
BrahimElhajji <elhajji080898@gmail.com>
2024-02-06 17:01:33 -0800

    task 10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Student class"""
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        """function that Instantiat with first_name, last_name and age"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """function that retrieves a dictionary representation
+        of a Student instance"""
+        if attrs is None:
+            return self.__dict__
+        else:
+            return {i: getattr(self, i) for i in attrs if hasattr(self, i)}
