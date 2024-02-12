8bf51e79e8e45bf8338b2679911e5f708be5127a
BrahimElhajji <elhajji080898@gmail.com>
2024-02-12 15:46:06 -0800

    rectangle file

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -4,6 +4,7 @@ from models.base import Base
 
 
 class Rectangle(Base):
+    """Rectangle Class"""
     def integer_validator(self, name, value):
         """a validation function for height and width"""
         if type(value) is not int:
