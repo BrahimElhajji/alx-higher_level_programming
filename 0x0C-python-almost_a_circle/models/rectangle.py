7c8dee35a0f2ab80088c0f1a8afda6c231cb95e5
BrahimElhajji <elhajji080898@gmail.com>
2024-02-12 16:37:06 -0800

    fixed update function

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -94,21 +94,14 @@ class Rectangle(Base):
                 self.id, self.x, self.y, self.width, self.height)
 
         def update(self, *args, **kwargs):
-            """function that assigns an argument to each attribute"""
-        if args:
-            if len(args) >= 1:
-                self.id = args[0]
-            if len(args) >= 2:
-                self.width = args[1]
-            if len(args) >= 3:
-                self.height = args[2]
-            if len(args) >= 4:
-                self.x = args[3]
-            if len(args) >= 5:
-                self.y = args[4]
-        elif kwargs:
-            for key, value in kwargs.items():
-                setattr(self, key, value)
+             """function that assigns an argument to each attribute"""
+             if args:
+                 list_args = ['id', 'width', 'height', 'x', 'y']
+                 for arg in range(len(args)):
+                     setattr(self, list_args[arg], args[arg])
+            else:
+                for key, value in kwargs.items():
+                    setattr(self, key, value)
 
     def to_dictionary(self):
         """function that returns the dictionary
