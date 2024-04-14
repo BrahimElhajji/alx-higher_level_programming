43b397a1bc359a7a70c1044e54c45fdb972873a2
BrahimElhajji <elhajji080898@gmail.com>
2024-04-14 09:16:42 -0700

    fixed 2 first tasks

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -7,7 +7,8 @@ starting with N (upper N) from the database hbtn_0e_0_usa """
 
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306,
-            user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+                         user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3])
 
     cursor = db.cursor()
 
