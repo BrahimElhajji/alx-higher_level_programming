e1003479239faab138e8572fb2d106e2ad588ead
BrahimElhajji <elhajji080898@gmail.com>
2024-04-25 18:26:55 -0700

    fixing task 3

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that takes in a URL and displays all HTTP methods the server will accept
-curl -s -X OPTIONS -I "$1" | grep -i "Allow:"
+curl -s -X OPTIONS -I "$1" | grep -i "Allow:" | cut -d ' ' -f 2-
