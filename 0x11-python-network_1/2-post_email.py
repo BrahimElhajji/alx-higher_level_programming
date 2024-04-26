2d264ea88eb98ee10260192801b472373062879a
BrahimElhajji <elhajji080898@gmail.com>
2024-04-26 12:49:48 -0700

    task 2 done

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -9,7 +9,7 @@ import sys
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    email = sys.argv[2] 
+    email = sys.argv[2]
 
     data = urllib.parse.urlencode({'email': email}).encode('utf-8')
 
